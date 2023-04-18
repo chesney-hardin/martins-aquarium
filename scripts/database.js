88b1b9505b6f52960f0821a3802af7b37b111bc4
Chesney Hardin <chesney.hardin@gmail.com>
2023-04-18 16:02:46 -0500

    initial html

---
+++ scripts/database.js
@@ -0,0 +1,69 @@
+//database is an object with a fish array of fish objects
+const database = {
+    fish: [
+        {
+            id: 1,
+            species: "Cherry Shrimp",
+            name: "",
+            length: "",
+            harvestLocation: "",
+            diet: "algae"
+        },
+        {
+            id: 2,
+            species: "Rainbow Shiner",
+            name: "",
+            length: "",
+            harvestLocation: "",
+            diet: "crustaceans"
+        },
+        {
+            id: 3,
+            species: "White Cloud Mountain Minnow",
+            name: "",
+            length: "",
+            harvestLocation: "",
+            diet: "crustaceans"
+        },
+        {
+            id: 4,
+            species: "Threadfin Rainbowfish",
+            name: "",
+            length: "",
+            harvestLocation: "",
+            diet: ["crustaceans", "algea"]
+        },
+        {
+            id: 5,
+            species: "African Butterfly Cichlid",
+            name: "",
+            length: "",
+            harvestLocation: "",
+            diet: ["crustaceans", "small insects"]
+        },
+        {
+            id: 6,
+            species: "Red-lined Torpedo Barb",
+            name: "",
+            length: "",
+            harvestLocation: "",
+            diet: "crustaceans"
+        },
+        {
+            id: 7,
+            species: "Royal Blue Tiger Pleco",
+            name: "",
+            length: "",
+            harvestLocation: "",
+            diet: "algea"
+        },
+        {
+            id: 8,
+            species: "",
+            name: "",
+            length: "",
+            harvestLocation: "",
+            diet: ""
+        }
+    ]
+}
