a3b260d445833d4aadbf229f145eef6124ca690f
Chesney Hardin <chesney.hardin@gmail.com>
2023-04-19 15:52:21 -0500

    big time chanegs with css

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -12,8 +12,8 @@ export const FishList = () => {
     for (const fish of fishes) {
 
         // Why is there a backtick used for this string?
-        htmlString += `<section class="fish card">
-            <div><img  class="fish__image image--card" src="${fish.image}" /></div>
+        htmlString += `<section class="fishCard">
+            <div><img class="fish__image" src="${fish.image}" /></div>
             <div class="fish__name">${fish.name}</div>
             <div class="fish__species">${fish.species}</div>
             <div class="fish__length">${fish.length}</div>
